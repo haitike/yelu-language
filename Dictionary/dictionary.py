c63d8ed5b2a19109c60600392172d7562260690f
haitike <haitike@gmail.com>
2016-04-27 20:55:14 +0200

    Delete first version

--- Dictionary/dictionary.py
+++ Dictionary/dictionary.py
@@ -1,7 +1,7 @@
 import shelve
 
 CONSONANTS = { "b" : "fire" ,
-         "c" : "evil",
+         "c" : "negative",
          "d" : "far",
          "f" : "fast",
          "g" : "nothing",
@@ -46,55 +46,71 @@ def get_root_meanings(root_string):
         meanings.append(CONSONANTS[i])
     return meanings
 
-##### MAIN ##########################
+#################### MAIN ####################
 d = shelve.open("Words/data")
 while True:
     option = input("\nSelect an option:\n"
-          "1 - Add Word\n"
-          "2 - Search (Yelu)\n"
-          "3 - Search (English)\n"
-          "4 - Delete Word\n"
+          "1 - Add Words\n"
+          "2 - Delete Word\n"
+          "3 - Search (Yelu)\n"
+          "4 - Search (English)\n"
           "5 - Browse Roots\n"
           "e - Exit\n")
 
     if option == "1":
-        new_word = input("Input a word: ")
-        if has_invalid_letter(new_word):
-            print("Some of the letters you introduced are not in Yelu alphabet")
-        else:
-            root_string = get_root_string(new_word)
-            if root_string:
-                print("Root: {}. Root meanings: {}".format(list(root_string), get_root_meanings(root_string)))
-                if root_string not in d:
-                    d[root_string] = dict()
-                if new_word not in d[root_string]:
-                    meaning = input("Now introduce the english meaning of <{}>: ".format(new_word))
-                    temp = d[root_string]
-                    temp[new_word] = meaning
-                    d[root_string] = temp
-                    print("Done. Word was added.")
-                else:
-                    overwrite = input("The word already exists. Do you want to overwrite it? (y/n) ")
-                    if overwrite == "y":
-                        meaning = input("Now introduce the english meaning of <{}>: ".format(new_word))
+        while True:
+            new_word = input("Input a word (<e> exit to Menu): ")
+            if new_word == "e":
+                break
+            if has_invalid_letter(new_word):
+                print("Some of the letters you introduced are not in Yelu alphabet")
+            else:
+                root_string = get_root_string(new_word)
+                if root_string:
+                    print("Root: {}. Root meanings: {}".format(list(root_string), get_root_meanings(root_string)))
+                    if root_string not in d:
+                        d[root_string] = dict()
+                    if new_word not in d[root_string]:
+                        meaning = input("Now introduce the english meaning of <{}> (<e> exit to Menu): ".format(new_word))
+                        if meaning == "e":
+                            break
                         temp = d[root_string]
                         temp[new_word] = meaning
                         d[root_string] = temp
                         print("Done. Word was added.")
                     else:
-                        print("word was not added")
-            else:
-                print("You introduced an empty word or a word without consonants")
-        input()
+                        overwrite = input("The word already exists. Do you want to overwrite it? (y/n) ")
+                        if overwrite == "y":
+                            meaning = input("Now introduce the english meaning of <{}> (<e> exit to Menu): ".format(new_word))
+                            if meaning == "e":
+                                break
+                            temp = d[root_string]
+                            temp[new_word] = meaning
+                            d[root_string] = temp
+                            print("Done. Word was added.")
+                        else:
+                            print("word was not added")
+                else:
+                    print("You introduced an empty word or a word without consonants")
 
     if option == "2":
-        print("search...")
+        word_to_delete = input("What word to delete (<e> exit to Menu): ")
+        if word_to_delete == "e":
+            pass
+        else:
+            input("This is a beta feature, to be improved... ")
+            root_string = get_root_string(word_to_delete)
+            temp = d[root_string]
+            del temp[word_to_delete]
+            d[root_string] = temp
 
     if option == "3":
         print("search...")
+        input()
 
     if option == "4":
-        print("delete...")
+        print("search...")
+        input()
 
     if option == "5":
         l = input("Write a radical and will show all words that contain it (Empty: Show all words)  ")
